bf5c14d1261294a66ca8318c3f16b0027e3fe4ba
samarasrs <smr.samara.srs@gmail.com>
2020-04-10 11:37:35 -0300

    atualização

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
 import pygame as pg
 import os
+from . import tools
+from . import constants as c
 
 
-
+#deixando o video centralizado
+os.environ['SDL_VIDEO_CENTERED']='1'
+#iniciando o pygame
+pg.init()
+#definindo os eventos permitidos de teclado (controles do jogo)
+pg.event.set_allowed([pg.KEYDOWN, pg.KEYUP, pg.QUIT])
+#colocando o titulo da tela
+pg.display.set_caption(c.TITULO)
+#iniciando uma tela
+TELA = pg.display.set_mode(c.TELA_TAMANHO)
+#recebe uma area retangular
+TELA_RECT = TELA.get_rect()
